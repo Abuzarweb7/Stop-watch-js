e2e3856d30f04dc8e14cb99ea98f29e4ebecd80f
hammad-sohail <hammadsohail733@gmail.com>
2025-08-21 12:57:39 +0500

    start fnc

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-  function startFnc
+  const hour = document.querySelector("#hour");
+  const minute =document.querySelector("#minute");
+  const second = document.querySelector("#second");
+
+function startFnc(){
+ let count = 0
+  setInterval(function(){
+  
+   
+if(count  > 9){
+  second.innerHTML = count++ 
+}
+else{
+  second.innerHTML = "0" + count++
+}
+
+  },1000)
+}
+function stopFnc(){
+  console.log("stop watch");
+clearInterval(setInterval);
+}
