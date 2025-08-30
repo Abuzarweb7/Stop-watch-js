83c3d347a16cb3a72d67125113f9f385183a66c2
hammad-sohail <hammadsohail733@gmail.com>
2025-08-30 15:25:32 +0500

    done

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
-  const hours = document.querySelector("#hour");
-  const minute =document.querySelector("#minute");
-  const second = document.querySelector("#second");
-let startWatch;
+
+let hours = document.querySelector("#hour");
+let minute = document.querySelector("#minute");
+let second = document.querySelector("#second");
+
+let satrt;
+
 let sec = 0;
-let min = 0 ;
-let hour = 0 ;
+let min = 0;
+let hour = 0;
 function startFnc(){
- 
-   startWatch = setInterval(function(){
-if(sec == 60){
-  sec = 0;
-  min++;
+satrt=setInterval (function(){
+    sec++
+if(sec === 60){
+sec = 0;
+min++
+
 }
-if(min == 60){
-  min = 0;
-  hour++;
+if(min === 60){
+min = 0;
+hour++
+}
+if(sec < 10){
+second.innerHTML = "0"  +  sec;
+} else{
+    second.innerHTML = sec;
+}
+
+ 
+if(min < 10){
+minute.innerHTML = "0" +  min;
+
+} else{
+minute.innerHTML = min;
+}
+if(hur < 10){
+hour.innerHTML = "0" +  hour;
+} else{
+    hours.innerHTML = hour;
+}
+},1000)
 }
 
 
- 
-  },1000)
-}
+
+
+
+
 function stopFnc(){
-  console.log("stop watch");
-clearInterval(startWatch);
+clearInterval (satrt)
+console.log("stop watch");
 
 }
-
 function resetFnc(){
-  console.log("Reset watch");
-count = 0;
- 
-
+    clearInterval(satrt);
+    sec = 0;
+    min = 0;
+    hour = 0;
 }
-
-
-
-
